48d3f8ed4606ba05ee3d40ad9f6b27d83a8608f2
rahulxagrawal <145319343+rahulxagrawal@users.noreply.github.com>
2024-10-21 21:59:07 +0530

    Add files via upload

---
+++ day 19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, X, Y;
+        cin >> N >> X >> Y;
+        if (Y % X == 0 && Y / X <= N) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
